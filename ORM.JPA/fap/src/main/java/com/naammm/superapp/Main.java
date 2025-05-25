2b1548fe3ce31b4e4fa9f794bf33337cb259d7f5
Naammm <tagiangnamttg@gmail.com>
2025-05-25 22:26:44 +0700

    update
    update

---
+++ ORM.JPA/fap/src/main/java/com/naammm/superapp/Main.java
@@ -0,0 +1,54 @@
+package com.naammm.superapp;
+
+
+import com.naammm.superapp.entity.Student;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        insertStudent(); //gọi hàm insert để thêm mới sinh viên vào CSDL
+        selectAllStudents(); //gọi hàm select để lấy hết sinh viên trong CSDL
+    }
+
+    //INSERT/TẠO MỚI SINH VIÊN
+    public static void insertStudent() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.giaolang.orm-PU"); //gửi thông số cấu hình Server, nhà thầu JPA: Hibernate, nhà thầu JBDC cho JPA class lo để tạo kết nối tới CSDL cụ thể SQL Server
+        EntityManager em = emf.createEntityManager();
+        //tạo ra 1 object dùng để quản lí các entity class - map ngang sang table. Class Student chịu sự quản lí của em/EntityManager
+        //em/EntityManager sẽ lo CRUD trên 1 table nào đó!
+        //qua mấy hàm huyền thoại: persit() find(), remove(), merge(), refresh(), clear(), detach()...
+        //Toàn chơi object, đằng sau là table bị ảnh hưởng
+
+        Student an = new Student("SE1", "An", 2000, 8.5);
+        Student binh = new Student("SE2", "Binh", 2001, 7.5);
+        Student cuong = new Student("SE3", "Cuong", 2002, 6.5);
+        //gọi sếp EntityMânge giúp CRUD
+        em.getTransaction().begin(); //bắt đầu giao dịch, bắt đầu làm việc với CSDL BẮT BUỘC PHẢI CÓ
+        em.persist(an); //thêm mới sinh viên An vào CSDL
+        em.persist(binh); //thêm mới sinh viên Binh vào CSDL
+        em.persist(cuong); //thêm mới sinh viên Cuong vào CSDL
+        em.getTransaction().commit(); //kết thúc giao dịch, kết thúc làm việc với CSDL BẮT BUỘC PHẢI CÓ
+
+        //SELECT KO CẦN, VÌ KO THAY ĐỔI TRẠNG THÁI TABLE
+        em.close();
+        emf.close(); //đóng kết nối tới CSDL, giải phóng tài nguyên
+    }
+    //SELECT * ĐỂ LẤY HẾT DATA
+    public static void selectAllStudents() {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("com.giaolang.orm-PU"); //gửi thông số cấu hình Server, nhà thầu JPA: Hibernate, nhà thầu JBDC cho JPA class lo để tạo kết nối tới CSDL cụ thể SQL Server
+        EntityManager em = emf.createEntityManager();
+        List<Student> result = em.createQuery("FROM Student", Student.class).getResultList();
+        System.out.println("The list òf students: ");
+        for (Student x : result) {
+            System.out.println(x);
+        }
+        //dùng biểu thức Lambda để in cx được
+
+        em.close();
+        emf.close();
+    }
+}
---
+++ ORM.JPA/fap/src/main/java/com/naammm/superapp/entity/Student.java
@@ -0,0 +1,82 @@
+package com.naammm.superapp.entity;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Nationalized;
+//import jakarta.persistence.Entity;
+//import jakarta.persistence.Id;
+//import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Student") //nếu không có khai báo này mặc định lấy tên class thành tên table!!!
+public class Student {
+
+    @Id
+    @Column(name ="Id")// nếu không có khai báo này thì mặc định lấy tên biến thành tên cột
+    private String id;//camelCase, id tự nhập
+
+    @Column(name = "Name", nullable = false, length = 50) //nullable = false nghĩa là không được null, length = 50 nghĩa là tối đa 50 ký tự
+    public String name;
+    @Nationalized // Nationalized để hỗ trợ Unicode, nếu không có thì sẽ bị lỗi khi nhập tiếng Việt
+    @Column(name = "YOB", nullable = false) // YOB: Year of Birth
+    private int yob;
+
+    @Column(name = "GPA", nullable = false) // GPA: Grade Point Average
+    private double gpa;
+
+    //Bắt buộc phải có constructor rỗng, constructor full tham số
+    //GET()/SET()/TO STRING()
+    //BOILER-PLATE, LOMBOK
+    //THÊM DEPENDENCY LOMBOK SAU
+
+    public Student() {
+    }
+
+    public Student(String id, String name, int yob, double gpa) {
+        this.id = id;
+        this.name = name;
+        this.yob = yob;
+        this.gpa = gpa;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getYob() {
+        return yob;
+    }
+
+    public void setYob(int yob) {
+        this.yob = yob;
+    }
+
+    public double getGpa() {
+        return gpa;
+    }
+
+    public void setGpa(double gpa) {
+        this.gpa = gpa;
+    }
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", yob=" + yob +
+                ", gpa=" + gpa +
+                '}';
+    }
+}
