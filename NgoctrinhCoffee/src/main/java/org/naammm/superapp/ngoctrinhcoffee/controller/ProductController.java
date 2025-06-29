0352be81ac2d3ad94126b4ec148c008874274fc3
Naammm <tagiangnamttg@gmail.com>
2025-06-29 16:28:01 +0700

    update

--- NgoctrinhCoffee/src/main/java/org/naammm/superapp/ngoctrinhcoffee/controller/ProductController.java
+++ NgoctrinhCoffee/src/main/java/org/naammm/superapp/ngoctrinhcoffee/controller/ProductController.java
@@ -3,9 +3,7 @@ package org.naammm.superapp.ngoctrinhcoffee.controller;
 import org.naammm.superapp.ngoctrinhcoffee.entity.Product;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,15 +18,50 @@ import java.util.List;
 @Controller
 public class ProductController {
 
-    //
-
-    @GetMapping("/products")
-    public String showProducts() {
-        // Trả về tên trang để hiển thị danh sách sản phẩm
-        return "products"; // Trang products.html sẽ được hiển thị
+    @GetMapping("/products/edit")
+    public String update(@RequestParam("name") String name,
+                         @RequestParam("price") String price,
+                            @RequestParam("id") String id,
+                         Model model) {
+        model.addAttribute("msg", "Đã update thành công");
+        return "result"; // Trả về tên trang để hiển thị form chỉnh sửa sản phẩm
     }
 
-    @RequestMapping("/ngoctrinh")
+    @GetMapping("/products/edit/{id}") //tấch url thành 2 phần, 1 cố định 1 thay đổi, phần thay đổi là path variable
+    public String showProductForm(Model model, @PathVariable("id") String id) {
+        // đã trích ra được id muốn xem chi tiết từ click hyperlink của user
+        //tạm thời mock
+        Product selectProduct;
+        if (id.equalsIgnoreCase("NTCF1")) {
+            selectProduct = new Product("NTCF1", "Cà phê Ngọc Trinh", 100_000);
+        } else if (id.equalsIgnoreCase("NTCF2")) {
+            selectProduct = new Product("NTCF2", "Trà sữa Ngọc Trinh", 50_000);
+        } else if (id.equalsIgnoreCase("NTCF3")) {
+            selectProduct = new Product("NTCF3", "Sinh tố Ngọc Trinh", 70_000);
+        } else {
+            selectProduct = new Product("UNKNOWN", "Sản phẩm không tồn tại", 0);
+        }
+//        new Product("NTCF1", "Cà phê Ngọc Trinh", 100_000);
+//        new Product("NTCF2", "Trà sữa Ngọc Trinh", 50_000);
+//        new Product("NTCF3", "Sinh tố Ngọc Trinh", 70_000);
+        //Throw vào model để Thymeleaf có thể lấy ra
+        model.addAttribute("product", selectProduct);
+        return "product-form";
+    }
+
+//    @GetMapping("/products")
+//    public String showProducts() {
+//        // Trả về tên trang để hiển thị danh sách sản phẩm
+//        return "products"; // Trang products.html sẽ được hiển thị
+//    }
+    //nguy hiểm 1 chút: url này dành cho cả GET/PUT/POST/.. miễn match url
+    //ta cần phân tách: hàm nào dành cho GET, hàm nào dành cho POST
+
+    //Cách viết chuẩn
+//    @RequestMapping(path = {"/ngoctrinh", "/products"}, method = RequestMethod.GET)
+    //Cách viết ngắn
+
+    @GetMapping(path = {"/ngoctrinh", "/products"})
     public String list(Model model) { //list nghĩa là show danh sách sản phẩm
                             // nằm trong trang products.html
         model.addAttribute("message", "Danh sách sản phẩm Ngọc Trinh Coffee");
